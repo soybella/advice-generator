742bb0f09ce6b36637f7cc90d501b47fdbb450be
Isabella <isabellaj.lizarde@gmail.com>
2023-12-05 01:57:28 -0800

    Updated design/layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import logo from "./logo.svg";
+import iconDice from "./images/icon-dice.svg";
+import patternDivider from "./images/pattern-divider-desktop.svg";
 import "./App.css";
 
 function App() {
+  console.log(patternDivider);
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> Hello from advice generator app</h1>
-      </header>
+      <div className="App-header">
+        <section className="container">
+          <p className="container advice-number"> Advice #117 </p>
+          <h4 className="container advice-container">
+            "It is easy to sit up and take notice, what's difficult is getting
+            up and taking action."
+          </h4>
+          <div>
+            <img
+              src={patternDivider}
+              alt="SVG pattern divider"
+              className="pattern-divider"
+            />
+          </div>
+          <div className="icon-dice-container">
+            <div className="icon-sphere">
+              <img src={iconDice} alt="SVG icon dice" className="icon-dice" />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
